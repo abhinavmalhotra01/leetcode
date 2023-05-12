54fef8f954b67c533b9817e1055609715b73255d
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-05-12 10:31:31 +0530

    Time: 62 ms (59.89%), Space: 42.8 MB (62.03%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,17 @@
+function once<T extends (...args: any[]) => any>(fn: T): 
+ ((...args: Parameters<T>) => ReturnType<T> | undefined) {
+     let c =0
+  return function (...args) {
+    if(c){return undefined}
+    c+=1
+    return (fn(...args))
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
