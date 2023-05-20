dbad46268726b6e9637eeddf580191c2f9007428
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-05-20 15:56:43 +0530

    Time: 75 ms (20.97%), Space: 43.4 MB (32.26%) - LeetHub

---
+++ 2676-throttle/2676-throttle.ts
@@ -0,0 +1,31 @@
+type F = (...args: any[]) => void
+
+function throttle(fn: F, t: number): F {
+  let timeoutInProgress = null;
+  let argsToProcess = null;
+  
+  const timeoutFunction = () => {
+    if (argsToProcess === null) {
+      timeoutInProgress = null; // enter the waiting phase
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  };
+
+  return function throttled(...args) {
+    if (timeoutInProgress) {
+      argsToProcess = args;
+    } else {
+      fn(...args); // enter the looping phase
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  }
+};
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
