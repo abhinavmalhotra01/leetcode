9cc23061caf9b66247a86475ec90fc5fcd977bba
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-05-30 16:53:30 +0530

    Time: 68 ms (40.43%), Space: 44.8 MB (30.85%) - LeetHub

---
+++ 2693-call-function-with-custom-context/2693-call-function-with-custom-context.ts
@@ -0,0 +1,17 @@
+declare global { 
+    interface Function {
+      callPolyfill(context: Record<any, any>, ...args: any[]): any;
+	}
+}
+
+Function.prototype.callPolyfill = function(context, ...args): any {
+    Object.defineProperty(context,'fn',{
+        value:this,
+    });
+    return context.fn(...args);
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
